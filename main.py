c8096ce30d307ae2a426669e2263a6e98c21ff01
Lucas <57031937+lukaqueres@users.noreply.github.com>
2023-08-18 11:57:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def ask_code() -> str:
     return input()
 
 
-client = odata.Client()
+client = odata.Client(source="codede", ssl_verify=False)
 
 
 @client.ready
 async def on_ready():
-    print(f"Logged in as {client.email} with {await client.latency()} ms latency")
+    print(f"Logged in as {client.email} with some ms latency")
 
 
 async def main():
@@ -27,13 +27,41 @@ async def main():
 
     # orders = await client.production.orders()
     # order = await client.production.order(7)
-    order = await client.production.order(10)
-    cancelled = await order.production_order.cancel()
+    print(await client.production_orders())
 
-    workflows = await client.workflows()
-    print(bool(order))
+    order = await client.production_order.get(10)
+
+    if order:
+        cancelled = await order.cancel()
+
+    workflows = await client.workflows("", "", "", top=2, count=True)
+    print(workflows.count)
+    for workflow in workflows:
+        print(f"{workflow.display_name} - {workflow.description}")
     await asyncio.sleep(900)
     await client.stop()
 
 
-asyncio.run(main())
+async def codede_tests():
+    await client.run(email=os.environ.get("COPERNICUS"),
+                     password=os.environ.get("COPERNICUS_PASS"),
+                     platform="copernicus")
+
+    await client.wait_until_ready()
+
+    products = await client.products("ContentDate/End lt 2019-05-15T00:05:00.000Z",
+                                     "",
+                                     top=1)
+
+    for product in products:
+        nodes = await product.nodes
+        print(f"Nodes of {product.name}:")
+        if nodes:
+            for node in nodes:
+                print(node.name)
+
+    await asyncio.sleep(900)
+    await client.stop()
+
+
+asyncio.run(codede_tests())
