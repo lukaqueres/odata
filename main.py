79293ca7dc6f54606fb6a82131e8c9a5bcfc4024
Lucas <57031937+lukaqueres@users.noreply.github.com>
2023-08-22 16:07:53 +0200

    New tests

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import asyncio
 import os
 
 import odata
+from odata import Collections, Filter, Attributes
 
-from odata._helpers import TimeConverter  # TODO: TMP FIND BETTER SOLUTION
+import datetime  # TODO: TMP
 
 
 def ask_code() -> str:
@@ -49,13 +50,20 @@ async def product_tests():
                      password=os.environ.get("COPERNICUS_PASS"),
                      platform="copernicus")
 
-    # await client.product.filter.collection("SENTINEL-1").and_.name_has("S1A").or_.name_is("S1A_EW_GRDM_1SDH_20220503T065105_20220503T065203_043043_0523C5_AC61.SAFE").top(30).get()
+    query = client.product.filter.where(Collections.is_in(Collections.SENTINEL_1, Collections.SENTINEL_2),
+                                        Filter.or_where(
+                                            Filter.Name.has("S1A"),
+                                            Filter.Name.exact("S1A_EW_GRDM_1SDH_20220503T065105_20220503T065203_043043_0523C5_AC61.SAFE")
+                                        ),
+                                        Filter.Publication.span(
+                                            datetime.datetime.now() - datetime.timedelta(days=120),
+                                            datetime.datetime.now()),
+                                        Filter.Geographic.point(odata.types.Coordinates(-0.53195770021, 28.6548783618)),
+                                        Attributes.satisfies(Attributes.OrbitNumber, "!=", 987)
+                                        ).get()
 
-    # start_date = TimeConverter.to_date("2019-05-15T00:00:00.000Z")
-    # end_date = TimeConverter.to_date("2019-05-16T00:00:00.000Z")
 
-    # await client.product.filter.geographic_point(-0.5319577002158441, 28.65487836189358).and_.sensing_date(start_date, end_date).count(True).expand("Attributes").get()
-
+    return
     collection = await client.product.get()
     # collection = await client.product.id("fe37ae5f-153b-511c-89b9-dcc059c86489", expand="Attributes")
 
