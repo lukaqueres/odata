02b02b3aed1cb6626bff49a848fb13e7ef0a8aa9
Lucas <57031937+lukaqueres@users.noreply.github.com>
2023-08-24 16:56:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-html_static_path = ['_static']
+html_static_path = ['docs/_static']
 
 html_css_files = ['custom.css',
                   ('custom.css', {'media': 'print'})]
